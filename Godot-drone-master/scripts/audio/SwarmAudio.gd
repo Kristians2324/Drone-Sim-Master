b3b57a1237ac75007c6b596fe075ca96cd45ff53
Kristian <kristiansbergmanis0@gmail.com>
2026-08-07 19:01:57 +0700

    Fixed Tab mode audio breaking ear drums

--- Godot-drone-master/scripts/audio/SwarmAudio.gd
+++ Godot-drone-master/scripts/audio/SwarmAudio.gd
@@ -23,12 +23,12 @@ func setup_swarm_audio() -> void:
 	if swarm_player_3d == null:
 		swarm_player_3d = AudioStreamPlayer3D.new()
 		swarm_player_3d.name = "SwarmAudio3D"
-		swarm_player_3d.unit_size = 2.0
-		swarm_player_3d.max_distance = 450.0
+		swarm_player_3d.unit_size = 25.0
+		swarm_player_3d.max_distance = 650.0
 		swarm_player_3d.attenuation_model = AudioStreamPlayer3D.ATTENUATION_INVERSE_DISTANCE
 		swarm_player_3d.doppler_tracking = AudioStreamPlayer3D.DOPPLER_TRACKING_PHYSICS_STEP
 		swarm_player_3d.panning_strength = 1.0
-		swarm_player_3d.volume_db = -80.0
+		swarm_player_3d.volume_db = -24.0
 		swarm_player_3d.bus = "Master"
 		if shared_swarm_stream:
 			swarm_player_3d.stream = shared_swarm_stream
@@ -70,14 +70,14 @@ func update_swarm_audio(center_pos: Vector3, avg_speed: float, count: int, delta
 		
 		swarm_player_3d.stream_paused = false
 
-		# Soft, quiet, gentle background swarm hum (-18 dB max)
+		# Clear, rich 3D spatial swarm soundscape (-8 dB max)
 		var speed_vol_boost = clampf(avg_speed / 20.0, 0.0, 1.0) * 6.0
-		var target_vol = clampf(-24.0 + speed_vol_boost, -28.0, -18.0)
+		var target_vol = clampf(-14.0 + speed_vol_boost, -16.0, -8.0)
 
 		var target_pitch = clampf(0.95 + (avg_speed / 30.0) * 0.10, 0.90, 1.15)
 
 		var dt = clampf(delta * 4.0, 0.03, 0.5)
-		var cur_vol = swarm_player_3d.volume_db if not is_nan(swarm_player_3d.volume_db) and not is_inf(swarm_player_3d.volume_db) else -80.0
+		var cur_vol = swarm_player_3d.volume_db if not is_nan(swarm_player_3d.volume_db) and not is_inf(swarm_player_3d.volume_db) else -24.0
 		var cur_pitch = swarm_player_3d.pitch_scale if not is_nan(swarm_player_3d.pitch_scale) and not is_inf(swarm_player_3d.pitch_scale) else 1.0
 
 		swarm_player_3d.volume_db = clampf(lerpf(cur_vol, target_vol, dt), -80.0, 6.0)
